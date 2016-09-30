dd44645069751c08a393ef4555fc99f555866eaa
hashrock <hashedrock@gmail.com>
2016-10-01 00:21:42 +0900

    feat(app): add vue

---
+++ ui/main.ts
@@ -0,0 +1,9 @@
+import {ViewMain} from "./view-main"
+import * as Vue from "vue"
+
+new Vue({
+    el: "main",
+    components: {
+        "view-main": ViewMain
+    }
+})
---
+++ ui/view-main.ts
@@ -0,0 +1,11 @@
+import Component from 'vue-class-component'
+import * as Vue from 'vue'
+@Component({
+  props: {
+  },
+  template: `
+    <div>Hello, Vue!</div>
+  `
+})
+export class ViewMain extends Vue {
+}
