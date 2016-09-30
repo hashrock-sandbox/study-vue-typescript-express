e3c12582058722cd91d36b7fa3ff46f995862382
hashrock <hashedrock@gmail.com>
2016-10-01 01:51:02 +0900

    feat(frontend): add ajax request

--- ui/src/view-main.ts
+++ ui/src/view-main.ts
@@ -1,11 +1,34 @@
 import Component from 'vue-class-component'
 import * as Vue from 'vue'
+import axios from "axios"
+
+interface ApiResponse{
+  msg: string
+}
+
 @Component({
   props: {
   },
   template: `
+  <div>
     <div>Hello, Vue!</div>
+    <h2>Ajax request</h2>
+    <div>msg: {{msg}}</div>
+  </div>
   `
 })
 export class ViewMain extends Vue {
+  msg: string
+
+  data():any{
+    return {
+      msg: ""
+    }
+  }
+  ready(){
+    axios.get("/api").then((res)=>{
+      var api:ApiResponse = res.data;
+      this.msg = api.msg
+    })
+  }
 }
